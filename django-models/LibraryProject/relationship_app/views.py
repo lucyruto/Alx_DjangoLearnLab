41f8f163ae98db6574f77f0679865a8f39a2664d
lucyruto <lucyruto.lr@gmail.com>
2025-09-28 02:39:34 +0300

    Adding files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from .models import Book
-from django.views.generic import DetailView
+# from django.views.generic import DetailView
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.generic.detail import DetailView
 
 
 # ---------------- Function-Based View ----------------
