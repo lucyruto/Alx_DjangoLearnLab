cc33cf99ab71e775f7e788cad3da9ba55a2849f6
lucyruto <lucyruto.lr@gmail.com>
2025-09-28 03:36:34 +0300

    Extending the User Model with a UserProfile

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,35 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
+
+# ---------------- Role-check helper functions ----------------
+# hasattr(user, "profile") ensures we don’t get errors if the user somehow doesn’t have a profile.
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, "profile") and user.profile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, "profile") and user.profile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, "profile") and user.profile.role == 'Member'
+
+
+# ---------------- Role-Based Views ----------------
+# Utilize the @user_passes_test decorator to check the user’s role before granting access to each view.
+@user_passes_test(is_admin, login_url='login')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian, login_url='login')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member, login_url='login')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
 # ---------------- Function-Based View ----------------
