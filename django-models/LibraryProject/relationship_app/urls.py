c64496a97e642b7b797bb78d63c0026b1e095920
lucyruto <lucyruto.lr@gmail.com>
2025-09-28 04:39:21 +0300

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,27 +1,20 @@
+# relationship_app/urls.py
+
 from django.urls import path
-from .views import book_list, LibraryDetailView
-from .views import (
-    register_view,
-    login_view,
-    logout_view,
-    book_list,
-    add_book,
-    edit_book,
-    delete_book,
-    LibraryDetailView,
-)
+from .views import list_books, LibraryDetailView
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # Book views
-    path("books/", book_list, name="book-list"),
-    path("books/add/", add_book, name="add-book"),
-    path("books/<int:book_id>/edit/", edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", delete_book, name="delete-book"),
-
-    # authentication views
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('login/<int:pk>/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/<int:pk>/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('books/', list_books, name='list_books'),  
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
+    path('books/add/', views.add_book, name="add_book/"),
+    path('books/edit/<int:book_id>/', views.edit_book, name="edit_book/"),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'), 
 ]
-
-
