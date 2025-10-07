bbd4e8ae6cc8d0a926547c268e8a0bd07322f6e9
lucyruto <lucyruto.lr@gmail.com>
2025-10-07 15:59:32 +0300

    Modifying serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,24 +2,25 @@ from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
+# Get the custom user model
 CustomUser = get_user_model()
 
 
-# User Serializer
+# ---------- User Serializer ----------
 class CustomUserSerializer(serializers.ModelSerializer):
-    following = serializers.SerializerMethodField()  # show users they follow
+    following = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'following']
-        read_only_fields = ['id','username','following']
+        read_only_fields = ['id', 'username', 'following']
 
     def get_following(self, obj):
-        # return a list of usernames the user follows
+        # Returns a list of usernames the user follows
         return [user.username for user in obj.following.all()]
 
 
-# Registration Serializer
+# ---------- Registration Serializer ----------
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
 
@@ -28,14 +29,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        # create_user handles password hashing automatically
+        # create_user() automatically handles password hashing
         user = CustomUser.objects.create_user(**validated_data)
-        # checks if user already has a token or not before creating one. does not crash when user is existing
-        token, created = Token.objects.get_or_create(user=user)
+        # Generate or get authentication token
+        Token.objects.get_or_create(user=user)
         return user
 
 
-# Login Serializer
+# ---------- Login Serializer ----------
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True, required=True)
@@ -44,11 +45,10 @@ class LoginSerializer(serializers.Serializer):
         username = attrs.get('username')
         password = attrs.get('password')
 
-        # Check credentials
         user = authenticate(username=username, password=password)
         if not user:
-            raise serializers.ValidationError('Invalid credentials')
+            raise serializers.ValidationError("Invalid credentials")
 
-        # Add user to validated data so views can access it
+        # Add the user to attrs so views can access it
         attrs['user'] = user
         return attrs
